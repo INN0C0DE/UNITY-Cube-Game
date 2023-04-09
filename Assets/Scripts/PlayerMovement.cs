fe482e410c3bc4d5765c2faf34e338c6eaa35b37
INN0C0DE <trickted@gmail.com>
2023-04-09 23:20:29 +0800

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    void FixedUpdate()
+    {
+        // add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // movement key
+        if ( Input.GetKey("d") ) 
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+
+
+
+}
